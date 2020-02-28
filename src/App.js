203a55df997c26d7a6460a1b0e29a36f6556e7b9
jufergom <jufergom@hotmail.com>
2020-02-28 16:33:00 -0600

    login is now connected with backend

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'antd/dist/antd.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LoginPage from './components/Login';
+import Menu from './components/Menu';
 
 const routing = (
   <BrowserRouter>
     <div>
       <Switch>
         <Route path="/" exact component={LoginPage}></Route>
+        <Route path="/home" component={Menu}></Route>
       </Switch>
     </div>
   </BrowserRouter>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,48 @@
 import React, { Component } from "react";
 import { Button, Input, Avatar } from 'antd';
+import { withRouter } from "react-router-dom";
 import './Styles/Login.css';
 
 class LoginPage extends Component {
     constructor(props){
         super(props);
         this.state={
+			username: '',
+			password: '',
             visible: false,
             data: 'default'
         }
-    }
+	}
+	
+	handleChange = (Name,event) => {
+    	this.setState({[Name]: event.target.value});
+	};
+	
+	checkAccount = () => {
+		let url = 'http://localhost:5000/api/login';
+        let data = {username: this.state.username, password: this.state.password};
+
+        fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers:{
+              'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if(res.status === 200) {
+				//let history = useHistory();
+
+				this.props.history.push("/home");
+            }
+            else {
+                alert('Usuario y/o contraseña incorrectos');
+            }
+        })
+        .catch(error => {
+			console.log(error);
+            alert('Ha ocurrido un error al iniciar sesion, '+error);
+        })
+	}
 
     render() {
         return(
@@ -26,22 +59,34 @@ class LoginPage extends Component {
 						<div className="login-form-row">
 							<Avatar src="https://lh3.googleusercontent.com/proxy/22TtDGVkrXXcOn1jQE0zqqY8b-L5VEvLrb1xx_eCchKKtPmkLrQv_0JH9D5psOxj8ujp9NPZ52C8-hOPzw7zn8PbNnoJJm_m2qoW17V3zEQGDyDuRxiSr70q9IeYGuUg" />
 							<div className="fixed-width"></div>
-							<Input />
+							<Input 
+								value={this.state.username}
+								onChange={this.handleChange.bind(this,'username')}
+							/>
 						</div>
 						<br/>
 						<br/>
 						<div className="login-form-row">
 							<Avatar src="https://conceptdraw.com/a155c4/p40/preview/640/pict--cloud-lock-cloud-round-icons-vector-stencils-library" />
 							<div className="fixed-width"></div>
-							<Input.Password />
+							<Input.Password 
+								value={this.state.password}
+								onChange={this.handleChange.bind(this,'password')}
+							/>
 						</div>
 					</Input.Group>
 					<br/>
-					<Button type="primary" className="login-button">Iniciar Sesión</Button>
+					<Button 
+						type="primary" 
+						className="login-button"
+						onClick={this.checkAccount.bind(this)}
+					>
+						Iniciar Sesión
+					</Button>
 				</div>
 			</div>
         );
     }
 }
 
-export default LoginPage;
+export default withRouter (LoginPage);
